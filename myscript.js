321c596bbf1e1769d445cb817935969c036e9f58
nagou967 <57115719+nagou967@users.noreply.github.com>
2022-03-15 07:52:47 +0900

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+$(function () {
+
+  // 固定文字
+  var myName = "Yuya Ushijima";
+  document.title = myName;  // タイトル
+  //document.querySelectorAll('header-logo')
+  const test = document.getElementsByClassName('gnav-item');
+  console.log(test)
+  //document.getElementsByClassName('name').textContent = myName;
+  
+});
